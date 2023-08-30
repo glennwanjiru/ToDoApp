c03769efc9ffec58af483bf44ff50cc749b88984
glennwanjiru <glennnjoroge@gmail.com>
2023-08-31 00:21:01 +0300

    commit

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,35 @@
+
+<div class="authContainer">
+    <form>
+        <h1>Login</h1>
+
+
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <label>
+            <input type="email" placeholder="email"/>
+        </label>
+        <label>
+            <input type="email" placeholder="email"/>
+        </label>
+    </form>
+
+</div>
+
+<style>
+    .authContainer
+     {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+
+    }
+</style>
