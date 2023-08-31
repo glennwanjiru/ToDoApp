7b7b9c3092413fc60f30ba2d6d1be8d132e6e5bc
glennwanjiru <glennnjoroge@gmail.com>
2023-08-31 23:59:42 +0300

    UP

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { deleteApp, getApps, initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,15 +7,24 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOOTwzGM-Y0eTTrwxeBW8PxwD_M5yzx0k",
-  authDomain: "todoapp-e7492.firebaseapp.com",
-  projectId: "todoapp-e7492",
-  storageBucket: "todoapp-e7492.appspot.com",
-  messagingSenderId: "510462614834",
-  appId: "1:510462614834:web:1094cd742f088c0c5bcb2b",
-  measurementId: "G-MXZYMRYDBV"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.VITE_AUTH_DOMAIN,
+  projectId:import.meta.VITE_PROJECT_ID,
+  storageBucket:import.meta.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.VITE_APP_ID,
+  measurementId: import.meta.VITE_MEASURING_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let firebaseApp;
+if (!getApps().length){
+    firebaseAp=initializeApp(firebaseConfig)
+
+}else{
+    firebaseApp=getApp()
+    deleteApp(firebaseApp)
+    firebaseApp=initializeApp(firebaseConfig)
+
+}
+export const db =getFirestore((firebaseApp))
